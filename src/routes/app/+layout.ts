6bc79bec4637bf590246c18b35abfc5d0ed45ea6
Konrad <konradm.studium@gmail.com>
2025-03-09 01:37:27 +0100

    Update app title and enhance user profile loading logic

--- src/routes/app/+layout.ts
+++ src/routes/app/+layout.ts
@@ -1,31 +1,34 @@
 import { goto } from "$app/navigation";
 import { user, userProfile } from "$lib/auth";
 import { supabase } from "$lib/supabase";
-
+import { get } from "svelte/store";
 
 /**
  * Protects the /app/* routes
  * Loads the user profile and user data
  */
 export async function load() {
-    let profile: any = null;
-    userProfile.subscribe((v) => profile = v)
-    let userData = null;
-    user.subscribe((v) => userData = v)
-
-
+    const userData = get(user);
     if (!userData) return goto("/")
 
-    if (profile != null && profile.displayname) return
+    const currentProfile = get(userProfile);
+    if (!currentProfile || !currentProfile.displayname) {
+        const { data: profileData } = await supabase
+            .from('user_profiles')
+            .select("displayname, settings")
+            .single();
+    
+        const { data: streakData } = await supabase
+        .from('user_streaks')
+        .select("streak")
+        .single();
 
-    const { data: profileData } = await supabase.from('user_profiles').select("displayname, settings").single()
-    const { data: streakData } = await supabase.from('user_streaks').select("streak").single()
+        if (!profileData) return
 
-    if (!profileData || !streakData) return
-
-    userProfile.set({
-        displayname: profileData.displayname,
-        settings: profileData.settings,
-        streak: streakData.streak,
-    })
+        userProfile.set({
+            displayname: profileData.displayname,
+            settings: profileData.settings,
+            streak: streakData ? streakData.streak : 0,
+        });
+    }
 }
