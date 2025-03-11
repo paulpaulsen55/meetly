fe2acc6602c5ca4b4c64a16b9fb6d06924db903e
Konrad <konradm.studium@gmail.com>
2025-03-11 19:16:23 +0100

    Implement upcoming events overview AND match design to figma

--- src/lib/components/WeekStreak.svelte
+++ src/lib/components/WeekStreak.svelte
@@ -3,7 +3,7 @@
     let activeDay = 0; // Monday
 </script>
   
-<div class="bg-gray-100 rounded-xl p-4 flex justify-between mb-4">
+<div class="bg-gray-50 rounded-3xl p-4 flex justify-between mb-4">
     {#each days as day, index}
         <div class={`w-10 h-10 flex items-center justify-center rounded-full 
         ${index === activeDay ? 'bg-blue-500 text-white' : 'text-gray-400'}`}>
