1d0b1396177352117d4135b6cdc90dc55e723a8c
paulpaulsen55 <paulpaulsenx@gmail.com>
2025-03-25 21:50:05 +0100

    remove console logs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,16 +4,12 @@ import { session, user, userProfile } from './stores'
 
 // Initial session
 supabase.auth.getSession().then(({ data }) => {
-    console.log("Initial session:", data);
-    
     session.set(data.session)
     user.set(data.session?.user || null)
 })
 
 // Listen for auth changes
 supabase.auth.onAuthStateChange(async (event, newSession) => {
-    console.log("Auth event:", event, newSession);
-    
     session.set(newSession)
     user.set(newSession?.user || null)
     
