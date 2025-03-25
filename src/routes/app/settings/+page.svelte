b6f4d124f76502666dd0dfe59ed04151559152ee
paulpaulsen55 <paulpaulsenx@gmail.com>
2025-03-25 21:49:06 +0100

    removed duplicated code

--- src/routes/app/settings/+page.svelte
+++ src/routes/app/settings/+page.svelte
@@ -1,17 +1,6 @@
 <script>
-    import { goto } from "$app/navigation";
-    import { user } from "$lib/stores";
-    import { loadProfile } from "$lib/helper";
     import { supabase } from "$lib/supabase";
     import { ChevronLeft } from "lucide-svelte";
-
-    user.subscribe((value) => {
-        if (!value) {
-            goto("/");
-        } else {
-            loadProfile();
-        }
-    });
 </script>
 
 <main class="h-screen flex flex-col bg-white p-4 max-w-md mx-auto">
@@ -23,14 +12,10 @@
         </a>
         <span>Settings<span> </span></span>
     </h1>
-    {#if $user}
-        <button
-            onclick={async () => supabase.auth.signOut()}
-            class="text-center py-2 px-4 rounded bg-blue-500 text-white font-bold hover:bg-blue-600 transition-colors"
-        >
-            Abmelden
-        </button>
-    {:else}
-        <p>Lädt...</p>
-    {/if}
+    <button
+        onclick={async () => supabase.auth.signOut()}
+        class="text-center py-2 px-4 rounded bg-blue-500 text-white font-bold hover:bg-blue-600 transition-colors"
+    >
+        Abmelden
+    </button>
 </main>
